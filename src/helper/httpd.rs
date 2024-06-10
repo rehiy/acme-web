20cbda4745187006a9b4496a1285f7a41719ab4f
若海 <wang@rehiy.com>
2024-06-10 12:17:40 +0800

    fix: 更改接口为POST请求

--- src/helper/httpd.rs
+++ src/helper/httpd.rs
@@ -1,6 +1,6 @@
 use crate::acme;
 use axum::{handler::HandlerWithoutStateExt, http::StatusCode};
-use axum::{routing::get, Router};
+use axum::{routing::post, Router};
 use std::fs::read_to_string;
 use tower_http::{services::ServeDir, trace::TraceLayer};
 
@@ -13,21 +13,21 @@ pub async fn listen() {
     axum::serve(listener, router).await.unwrap();
 }
 
-fn handler() -> Router {
-    let not_found = handler_404.into_service();
-    let serve_dir = ServeDir::new("public").not_found_service(not_found);
-
-    Router::new()
-        .fallback_service(serve_dir)
-        .route("/acme/info", get(acme::info::handler))
-        .route("/acme/list", get(acme::list::handler))
-}
-
-async fn handler_404() -> (StatusCode, String) {
+async fn error_404() -> (StatusCode, String) {
     let content = match read_to_string("public/404.html") {
         Ok(html) => html,
         Err(_) => String::from("404 Not Found"),
     };
 
     (StatusCode::NOT_FOUND, content)
+}
+
+fn handler() -> Router {
+    let not_found = error_404.into_service();
+    let serve_dir = ServeDir::new("public").not_found_service(not_found);
+
+    Router::new()
+        .fallback_service(serve_dir)
+        .route("/acme/info", post(acme::info::handler))
+        .route("/acme/list", post(acme::list::handler))
 }

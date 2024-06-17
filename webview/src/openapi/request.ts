7b162e5a41c94e7f661d09d6a123af9b19546d3f
若海 <wang@rehiy.com>
2024-06-17 21:58:21 +0800

    feat: 增加acme接口客户端实现

--- webview/src/openapi/request.ts
+++ webview/src/openapi/request.ts
@@ -1,31 +1,49 @@
 export class Acme {
     constructor() { }
 
-    info() {
-        let data = { action: 'info' };
+    public list() {
+        const data = { action: 'list' };
         return this.request('/acme', { body: JSON.stringify(data) });
     }
 
-    list() {
-        let data = { action: 'list' };
+    public issue(opts: IssueOptions) {
+        const data = { action: 'issue', ...opts };
         return this.request('/acme', { body: JSON.stringify(data) });
     }
 
-    issue(opts: IssueOptions) {
-        let data = { action: 'issue', ...opts };
+    // 账号相关接口
+
+    public info() {
+        const data = { action: 'info' };
         return this.request('/acme', { body: JSON.stringify(data) });
     }
 
-    async request(input: string, options: RequestInit = {}) {
+    public registerAccount(opts: RegisterAccountOptions) {
+        const data = { action: 'register-account', ...opts };
+        return this.request('/acme', { body: JSON.stringify(data) });
+    }
+
+    public updateAccount(email: string) {
+        const data = { action: 'update-account', email };
+        return this.request('/acme', { body: JSON.stringify(data) });
+    }
+
+    public setDefaultCA(server: string) {
+        const data = { action: 'set-default-ca', server };
+        return this.request('/acme', { body: JSON.stringify(data) });
+    }
+
+    // 私有工具函数
+
+    private async request(input: string, options: RequestInit = {}) {
+        // 设置 Header
         const headers: Record<string, string> = {
             'Content-Type': 'application/json'
         };
-        // 设置 Token
         const token = sessionStorage.getItem('token');
         if (token && token.trim().length > 1) {
             headers['Authorization'] = `Bearer ${token}`;
         }
-        // 设置 Header
         options.headers = Object.assign(headers, options.headers);
         // 发起 HTTP 请求
         try {
@@ -54,4 +72,10 @@ export class Acme {
 export interface IssueOptions {
     domain: Array<string>;
     dns: string;
+}
+
+export interface RegisterAccountOptions {
+    server: string;
+    "eab-kid"?: string;
+    "eab-hmac-key"?: string;
 }
